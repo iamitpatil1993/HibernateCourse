0f2da462a30ed1762a01791cee35fe7be440eacd
Amit Patil <iamitpatil1993@gmail.com>
2018-05-13 17:05:31 +0530

    Hibernate Native derived property demo

--- src/test/java/com/example/hibernate/derivedproperty/DerivedPropertyDemoTest.java
+++ src/test/java/com/example/hibernate/derivedproperty/DerivedPropertyDemoTest.java
@@ -1,5 +1,7 @@
 package com.example.hibernate.derivedproperty;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.example.hibernate.model.identity.hibernate.BaseTest;
@@ -11,11 +13,20 @@ public class DerivedPropertyDemoTest extends BaseTest {
 		DerivedPropertyDemo demo = new DerivedPropertyDemo();
 		em.persist(demo);
 	}
-	
-	
+
 	@Test
 	public void test2() {
 		DerivedPropertyDemo demo = em.find(DerivedPropertyDemo.class, 1L);
 		System.out.println("derived field before SELECT :: " + demo.getDerivedField());
+	}
+
+	@Test
+	public void test3() {
+		List<DerivedPropertyDemo> results = em
+				.createQuery("select d from DerivedPropertyDemo d", DerivedPropertyDemo.class).getResultList();
+		results.forEach(demo -> {
+			System.out.println("original :: " + demo.getId() + " --->  derived :: " + demo.getDerivedField());
+
+		});
 	}
 }
